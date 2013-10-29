b1a4eba0f9dee348dea3876609e727780e3839ac
Josh Billingham <wjbillin@umich.edu>
2013-10-29 16:44:17 -0400

    xcode jazz and some executable stuff

---
+++ proj4/eecs381proj4/eecs381proj4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  eecs381proj4
+//
+//  Created by William Joshua Billingham on 10/29/13.
+//  Copyright (c) 2013 William Joshua Billingham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
