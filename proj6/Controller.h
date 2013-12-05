65c4bd4d10aaaeace850e0c0f1ec55d27b3a7cbf
Otto Sipe <ottosipe@gmail.com>
2013-12-04 21:25:44 -0500

    2 spacing

--- proj6/Controller.h
+++ proj6/Controller.h
@@ -19,55 +19,55 @@ class Agent;
 class Controller {
 public:
 	Controller();
-    
+  
 	// create View object, run the program by
-    // acccepting user commands, then destroy View object
+  // acccepting user commands, then destroy View object
 	void run();
-    
+  
 private:
-    //TODO: make sure std::function is Kosher, its much cleaner though
-    using CmdFunc_t = std::map<std::string, std::function<void(Controller*)>>;
-    using CmdFunc_Agent_t = std::map<std::string, std::function<void(Controller*, std::shared_ptr<Agent>)>>;
-    
-    using viewPair_t = struct {
-        std::string name;
-        std::shared_ptr<View> view;
-    };
-    using Views_t = std::list<viewPair_t>;
-    
-    // view commands
-    void view_open();
-    void view_close();
-    void view_default();
-    void view_size();
-    void view_zoom();
-    void view_pan();
-    
-    // return an iterator to the view, or throw
-    Views_t::iterator get_view_itr(const std::string &name);
-    // return the map view, or throw
-    std::shared_ptr<FullMapView> get_map_view();
-    // view factory
-    std::shared_ptr<View> create_view(const std::string& name);
-    
-    // whole-program commands
-    void prog_status();
-    void prog_show();
-    void prog_go();
-    void prog_build();
-    void prog_train();
-    
-    // agent commands
-    void agent_move(std::shared_ptr<Agent>);
-    void agent_work(std::shared_ptr<Agent>);
-    void agent_attack(std::shared_ptr<Agent>);
-    void agent_stop(std::shared_ptr<Agent>);
-    
-    // containers
-    CmdFunc_t program_cmds;
-    CmdFunc_Agent_t agent_cmds;
-    CmdFunc_t view_cmds;
-    Views_t views;
+  //TODO: make sure std::function is Kosher, its much cleaner though
+  using CmdFunc_t = std::map<std::string, std::function<void(Controller*)>>;
+  using CmdFunc_Agent_t = std::map<std::string, std::function<void(Controller*, std::shared_ptr<Agent>)>>;
+  
+  using viewPair_t = struct {
+    std::string name;
+    std::shared_ptr<View> view;
+  };
+  using Views_t = std::list<viewPair_t>;
+  
+  // view commands
+  void view_open();
+  void view_close();
+  void view_default();
+  void view_size();
+  void view_zoom();
+  void view_pan();
+  
+  // return an iterator to the view, or throw
+  Views_t::iterator get_view_itr(const std::string &name);
+  // return the map view, or throw
+  std::shared_ptr<FullMapView> get_map_view();
+  // view factory
+  std::shared_ptr<View> create_view(const std::string& name);
+  
+  // whole-program commands
+  void prog_status();
+  void prog_show();
+  void prog_go();
+  void prog_build();
+  void prog_train();
+  
+  // agent commands
+  void agent_move(std::shared_ptr<Agent>);
+  void agent_work(std::shared_ptr<Agent>);
+  void agent_attack(std::shared_ptr<Agent>);
+  void agent_stop(std::shared_ptr<Agent>);
+  
+  // containers
+  CmdFunc_t program_cmds;
+  CmdFunc_Agent_t agent_cmds;
+  CmdFunc_t view_cmds;
+  Views_t views;
 };
 
 #endif
--- proj6/Views.cpp
+++ proj6/Views.cpp
@@ -37,149 +37,149 @@ MapView::~MapView() {}
 // If the name is already present,the new location replaces the previous one.
 void MapView::update_location(const string& name, Point location)
 {
-    objects[name] = location;
+  objects[name] = location;
 }
 
 // Remove the name and its location; no error if the name is not present.
 void MapView::update_remove(const string& name)
 {
-    auto itr = objects.find(name);
-    if (itr != objects.end()) objects.erase(itr);
+  auto itr = objects.find(name);
+  if (itr != objects.end()) objects.erase(itr);
 }
 
 // Discard the saved information - drawing will show only a empty pattern
 void MapView::clear()
 {
-    objects.clear();
+  objects.clear();
 }
 
 // modify the display parameters
 // if the size is out of bounds will throw
 void MapView::set_size(int size_)
 {
-    if (size_ > MAX_SIZE) {
-        throw Error("New map size is too big!");
-    } else if (size_ <= MIN_SIZE) {
-        throw Error("New map size is too small!");
-    }
-    size = size_;
+  if (size_ > MAX_SIZE) {
+    throw Error("New map size is too big!");
+  } else if (size_ <= MIN_SIZE) {
+    throw Error("New map size is too small!");
+  }
+  size = size_;
 }
 
 // If scale is not postive, will throw Error("New map scale must be positive!");
 void MapView::set_scale(double scale_)
 {
-    if (scale_ <= 0.0) {
-        throw Error("New map scale must be positive!");
-    }
-    scale = scale_;
+  if (scale_ <= 0.0) {
+    throw Error("New map scale must be positive!");
+  }
+  scale = scale_;
 }
 
 // any values are legal for the origin
 void MapView::set_origin(Point origin_)
 {
-    origin = origin_;
+  origin = origin_;
 }
 
 // set the parameters to the default values
 void MapView::set_defaults()
 {
-    size = DEFAULT_SIZE;
-    scale = DEFAULT_SCALE;
-    origin = DEFAULT_ORIGIN;
+  size = DEFAULT_SIZE;
+  scale = DEFAULT_SCALE;
+  origin = DEFAULT_ORIGIN;
 }
 
 void MapView::draw()
 {
-    Grid_t grid;
-    print_header();
-    init_grid_data(grid);
-    print_grid(grid);
+  Grid_t grid;
+  print_header();
+  init_grid_data(grid);
+  print_grid(grid);
 }
 
 void MapView::print_header()
 {
-    cout << "Display size: " << size << ", scale: ";
-    cout << scale << ", origin: " << origin << endl;
+  cout << "Display size: " << size << ", scale: ";
+  cout << scale << ", origin: " << origin << endl;
 }
 
 void MapView::print_off_map(vector<string> &off)
 {
-    for (auto itr = off.begin(); itr != off.end(); itr++) {
-        if (itr != off.begin()) {
-            cout << ", ";
-        }
-        cout << *itr;
+  for (auto itr = off.begin(); itr != off.end(); itr++) {
+    if (itr != off.begin()) {
+      cout << ", ";
     }
-    if (off.size()) {
-        cout << " outside the map" << endl;
-    }
+    cout << *itr;
+  }
+  if (off.size()) {
+    cout << " outside the map" << endl;
+  }
 }
 
 MapView::Grid_t MapView::make_grid(int size)
 {
-    Grid_t grid(size);
-    for (auto &row : grid) {
-        row.resize(size);
-        for (auto &col : row) {
-            col = BLANK_CORDINATE;
-        }
+  Grid_t grid(size);
+  for (auto &row : grid) {
+    row.resize(size);
+    for (auto &col : row) {
+      col = BLANK_CORDINATE;
     }
-    return grid;
+  }
+  return grid;
 }
 
 void MapView::init_grid_data(Grid_t &grid)
 {
-    // setup grid
-    grid = make_grid(size);
-    
-    // init cells of grid
-    vector<string> off_map;
-    for (auto &i : objects) {
-        int ix, iy;
-        if(get_subscripts(ix, iy, i.second)) {
-            if(grid[iy][ix] == BLANK_CORDINATE) {
-                grid[iy][ix] = i.first.substr(0,2);
-            } else {
-                // unit is already here, show *
-                grid[iy][ix] = MULTI_CORDINATE;
-            }
-        } else { // not on map
-            off_map.push_back(i.first);
-        }
+  // setup grid
+  grid = make_grid(size);
+  
+  // init cells of grid
+  vector<string> off_map;
+  for (auto &i : objects) {
+    int ix, iy;
+    if(get_subscripts(ix, iy, i.second)) {
+      if(grid[iy][ix] == BLANK_CORDINATE) {
+        grid[iy][ix] = i.first.substr(0,2);
+      } else {
+        // unit is already here, show *
+        grid[iy][ix] = MULTI_CORDINATE;
+      }
+    } else { // not on map
+      off_map.push_back(i.first);
     }
-    
-    print_off_map(off_map);
+  }
+  
+  print_off_map(off_map);
 }
 
 // print the provided grid, regardless of view type
 void MapView::print_grid(const Grid_t &grid)
 {
-    // dont print decimal points
-    auto old_prec = cout.precision();
-    cout.precision(0);
-    for (int i = size-1; i >= 0; --i) {
-        // y axis labels
-        if(i % 3 == 0) {
-            double label = round(i * scale + origin.y);
-            cout << setw(4) << label << " ";
-        } else cout << "     ";
-        for (auto &str : grid[i]) {
-            cout << setw(2) << str;
-        }
-        cout << endl;
-    }
-    
-    // label x axis
-    for (int i = 0; i < size; ++i) {
-        if (i % 3 == 0) {
-            double label = round(i * scale + origin.x);
-            cout << "  " << setw(4) << label;
-        }
+  // dont print decimal points
+  auto old_prec = cout.precision();
+  cout.precision(0);
+  for (int i = size-1; i >= 0; --i) {
+    // y axis labels
+    if(i % 3 == 0) {
+      double label = round(i * scale + origin.y);
+      cout << setw(4) << label << " ";
+    } else cout << "     ";
+    for (auto &str : grid[i]) {
+      cout << setw(2) << str;
     }
     cout << endl;
-    
-    // restore precision
-    cout.precision(old_prec);
+  }
+  
+  // label x axis
+  for (int i = 0; i < size; ++i) {
+    if (i % 3 == 0) {
+      double label = round(i * scale + origin.x);
+      cout << "  " << setw(4) << label;
+    }
+  }
+  cout << endl;
+  
+  // restore precision
+  cout.precision(old_prec);
 }
 
 // Calculate the cell subscripts corresponding to the supplied location parameter,
@@ -190,46 +190,46 @@ void MapView::print_grid(const Grid_t &grid)
 // Return true if the location is within the grid, false if not
 bool MapView::get_subscripts(int &ix, int &iy, Point location) const
 {
-    // adjust with origin and scale
+  // adjust with origin and scale
 	Cartesian_vector subscripts = (location - origin) / scale;
 	// truncate coordinates to integer after taking the floor
 	// floor function will return the largest integer smaller than the supplied value
 	// even for negative values, so -0.05 => -1., which will be outside the array.
-    
+  
 	ix = int(floor(subscripts.delta_x));
 	iy = int(floor(subscripts.delta_y));
 	// if out of range, return false
 	if ((ix < 0) || (ix >= size) || (iy < 0) || (iy >= size)) {
 		return false;
-    } else
+  } else
 		return true;
 }
 
 // default constructor sets the default size, scale, and origin
 FullMapView::FullMapView()
 {
-    set_defaults();
+  set_defaults();
 }
 
 LocalView::LocalView(string name) : obj_name(name)
 {
-    set_scale(DEFAULT_LOCAL_SCALE);
-    set_size(DEFAULT_LOCAL_SIZE);
+  set_scale(DEFAULT_LOCAL_SCALE);
+  set_size(DEFAULT_LOCAL_SIZE);
 }
 
 void LocalView::update_location(const std::string& name, Point location)
 {
-    MapView::update_location(name, location);
-    if (name == obj_name) {
-        location.x -= LOCAL_ORIGIN_OFFSET;
-        location.y -= LOCAL_ORIGIN_OFFSET;
-        set_origin(location);
-    }
+  MapView::update_location(name, location);
+  if (name == obj_name) {
+    location.x -= LOCAL_ORIGIN_OFFSET;
+    location.y -= LOCAL_ORIGIN_OFFSET;
+    set_origin(location);
+  }
 }
 
 void LocalView::print_header()
 {
-    cout << "Local view for: " << obj_name << endl;
+  cout << "Local view for: " << obj_name << endl;
 }
 
 
@@ -241,30 +241,30 @@ ListView::~ListView() {} // explicit dtor
 // Notify for removal of object
 void ListView::update_remove(const string& name)
 {
-    objects.erase(name);
+  objects.erase(name);
 }
 
 // prints out the view
 void ListView::draw()
 {
-    cout << "Current " << view_name() << ":" << endl;
-    cout << "--------------" << endl;
-    for (auto i : objects) {
-        cout << i.first << ": " << i.second << endl;
-    }
-    cout << "--------------" << endl;
+  cout << "Current " << view_name() << ":" << endl;
+  cout << "--------------" << endl;
+  for (auto i : objects) {
+    cout << i.first << ": " << i.second << endl;
+  }
+  cout << "--------------" << endl;
 }
 
 // discards the view's contents
 void ListView::clear()
 {
-    objects.clear();
+  objects.clear();
 }
 
 // update the generic value held by the ListView
 void ListView::update_value(const string& name, double value)
 {
-    objects[name] = value;
+  objects[name] = value;
 }
 
 // Health View //
@@ -272,12 +272,12 @@ void ListView::update_value(const string& name, double value)
 // update the unit's health
 void HealthView::update_health(const string& name, int health)
 {
-    update_value(name, health);
+  update_value(name, health);
 }
 
 string HealthView::view_name()
 {
-    return HEALTH_VIEW_NAME;
+  return HEALTH_VIEW_NAME;
 }
 
 // Amounts View //
@@ -285,12 +285,12 @@ string HealthView::view_name()
 // update the amount of food
 void AmountsView::update_amount(const string& name, double amount)
 {
-    update_value(name, amount);
+  update_value(name, amount);
 }
 
 string AmountsView::view_name()
 {
-    return AMOUNTS_VIEW_NAME;
+  return AMOUNTS_VIEW_NAME;
 }
 
 
