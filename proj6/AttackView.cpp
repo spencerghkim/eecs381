ecbffddc5b06a3fff819aef19ef11eab1f320880
Otto Sipe <ottosipe@gmail.com>
2013-12-07 18:07:08 -0500

    fixed file location error, added demos

---
+++ proj6/AttackView.cpp
@@ -0,0 +1,100 @@
+
+#include "AttackView.h"
+
+#include "Geometry.h"
+
+#include <iostream>
+#include <set>
+
+using std::cout; using std::endl;
+using std::string;
+using std::set;
+
+// number of dots per unit of distance
+const int DISTANCE_MULT = 4;
+const string DOT = ". ";
+const string ATTACK_SYMBOL_LEFT = " > ";
+const string ATTACK_SYMBOL_RIGHT = "< ";
+
+
+// a new attack has started, keep track of it
+void AttackView::update_attack(const std::string& name, const std::string& target)
+{
+  attackers[name] = target;
+}
+
+// an attack has ended, remove it
+void AttackView::update_end_attack(const std::string& name)
+{
+  attackers.erase(name);
+}
+
+// an agent involved in an attack has lost health
+void AttackView::update_health(const std::string& name, int health)
+{
+  healths[name] = health;
+}
+
+// an agent involved in an attack has moved
+void AttackView::update_location(const std::string &name, Point location)
+{
+  locations[name] = location;
+}
+
+// Notify for removal of object
+void AttackView::update_remove(const std::string& name)
+{
+  attackers.erase(name);
+  healths.erase(name);
+  locations.erase(name);
+}
+
+// prints out the view
+void AttackView::draw()
+{
+  if (!attackers.size()) {
+    cout << "No Active Duels!" << endl;
+    return;
+  }
+  
+  cout << "Current Duels:" << endl;
+  set<string> printed;
+  
+  for (auto &pair : attackers) {
+    auto &attacker = pair.first;
+    auto &target = pair.second;
+    auto a_health = healths[attacker];
+    auto t_health = healths[target];
+    
+    // we already printed the other side of this duel
+    if (printed.find(attacker) != printed.end()) continue;
+    
+    // get the truncated distance between the duel
+    int dist = cartesian_distance(locations[attacker], locations[target]) * DISTANCE_MULT;
+    
+    cout << "(" << a_health << ") " << attacker << ATTACK_SYMBOL_LEFT;
+    
+    // print dots to represent distance in 2d
+    for (int i = 0; i < dist; ++i) {
+      cout << DOT;
+    }
+    
+    auto itr = attackers.find(target);
+    // check if our target is attacking us back
+    if (itr != attackers.end() && itr->second == attacker) {
+      // remember the attacking targets we've already printed
+      printed.insert(itr->first);
+      cout << ATTACK_SYMBOL_RIGHT;
+    }
+    cout << target << " (" << t_health << ")"  << endl;
+    
+  }
+}
+
+// discards the view's contents
+void AttackView::clear()
+{
+  attackers.clear();
+  healths.clear();
+  locations.clear();
+}
---
+++ proj6/AttackView.h
@@ -0,0 +1,52 @@
+#ifndef ATTACKVIEW_H_
+#define ATTACKVIEW_H_
+
+/*
+ 
+ * AttackView class *
+ 
+ TODO: blurb
+ */
+
+#include "View.h"
+
+#include <string>
+#include <map>
+
+class AttackView : public View { //TODO: design though, is it worth trying to inherit from ListView?
+public:
+  
+  // a new attack has started, keep track of it
+  void update_attack(const std::string& name, const std::string& target) override;
+  
+  // an attack has ended, remove it
+	void update_end_attack(const std::string& name) override;
+  
+  // an agent involved in an attack has lost health
+  void update_health(const std::string& name, int health) override;
+  
+  // an agent involved in an attack has moved
+  void update_location(const std::string& name, Point location) override;
+  
+  // Notify for removal of object
+	virtual void update_remove(const std::string& name) override;
+  
+  // prints out the view
+  void draw() override;
+	
+	// discards the view's contents
+	void clear() override;
+  
+protected:
+  using Attckers_t = std::map<std::string, std::string>;
+  using Healths_t = std::map<std::string, int>;
+  using Locations_t = std::map<std::string, Point>;
+  
+private:
+  
+  Attckers_t attackers;
+  Healths_t healths;
+  Locations_t locations;
+};
+
+#endif
